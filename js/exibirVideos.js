70ed87872ad5b65d46c0d5d290a7b9d26c08dcb5
ledsouza <leandro.souza.159@gmail.com>
2024-05-24 16:19:43 -0300

    preparando ambiente

---
+++ js/exibirVideos.js
@@ -0,0 +1,38 @@
+import { requestAPI } from "./requestAPI.js";
+
+const listaVideos = document.querySelector("[data-lista]");
+
+listarVideos();
+
+async function listarVideos() {
+    const videos = await requestAPI.getVideos();
+    videos.forEach((video) => {
+        const cardVideo = criarCardVideo(video);
+        listaVideos.appendChild(cardVideo);
+    });
+}
+
+function criarCardVideo(video) {
+    const itemVideo = document.createElement("li");
+    itemVideo.className = "videos__item";
+    itemVideo.innerHTML = `
+        <iframe
+            width="100%"
+            height="72%"
+            src="${video.url}"
+            title="${video.titulo}"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+        ></iframe>
+        <div class="descricao-video">
+            <img
+                src="${video.imagem}"
+                alt="Logo do canal Alura Cursos Online"
+            />
+            <h3>${video.titulo}</h3>
+            <p>${video.descricao}</p>
+        </div>
+    `;
+    return itemVideo;
+}
